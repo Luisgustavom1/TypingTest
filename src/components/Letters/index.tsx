38dc81c54a59fb76b862c74035820b83c5a3ab52
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-11-23 12:00:13 -0300

    Logica para trocar as linhas

--- src/components/Letters/index.tsx
+++ src/components/Letters/index.tsx
@@ -3,21 +3,34 @@ import React, { useEffect, useState } from 'react';
 import './styles.scss';
 
 const Letters: React.FC = () => {
+  const [text, setText] = useState('Aqui vai a palavra para testarmos nossa velocidade de digitação. Basta ir copiando esse texto. Aqui vai a palavra para testarmos nossa velocidade de digitação. Basta ir copiando esse texto. AAAAAAAAAAAAAAAAA dsfdsf sdfsdfsf dsf sdfs fdsf  sad asdasdasdasdas asd as as s s f l dasdbasodnasoidnaiod naid nasido naibdvfvsdbiso baw cdfasiudaowidcn bsvfuiasohdfpvvbipcau fscydshpgdb gpuiewoacnmpesfinacyv  fbansdcna ovyciaud')
   const [focado, setFocado] = useState(true)
   const [lettersClicked, setLettersClicked] = useState<string[]>([]);
   const [error, setError] = useState(false);
   const letterInput = document.querySelector('.letter-input') as HTMLInputElement
-  const textContent = document.querySelector('.letters-box')?.textContent
+
+  const nextLine = (index: number) => {
+    setText(text?.split('').slice(index + 57).join(''))
+    setLettersClicked(lettersClicked?.slice(index + 57))
+  }
 
   const letterClick = (e) => {
     console.log(e.charCode, 'tecla');
     const letterClicked = String.fromCharCode(e.charCode)
 
-    if (textContent?.split('')[lettersClicked.length] === letterClicked) {
+    if (text?.split('')[lettersClicked.length] === letterClicked) {
       const lettersClickedUpdate = lettersClicked
       lettersClickedUpdate.push(letterClicked)
       setLettersClicked([...lettersClickedUpdate])
+
       error && setError(false)
+
+      if ((lettersClickedUpdate.length / 57) % 2 === 0) {
+        console.log('addsad', lettersClickedUpdate.length % 57);
+        
+        nextLine(0)
+      }
+
       return;
     }
 
@@ -44,7 +57,7 @@ const Letters: React.FC = () => {
     <div className='letters-container'>
       <p className='letters letters-box' onClick={() => letterInput?.focus()}>
         <input onKeyPress={letterClick} readOnly onBlur={loseFocusInput} onFocus={focusOnInput} autoFocus className='letter-input'/>
-        Aqui vai a palavra para testarmos nossa velocidade de digitação. Basta ir copiando esse texto. Aqui vai a palavra para testarmos nossa velocidade de digitação. Basta ir copiando esse texto. AAAAAAAAAAAAAAAAA dsfdsf sdfsdfsf dsf sdfs fdsf  sad asdasdasdasdas asd as as s s f l
+          {text}
         <strong className={`letters letters-typed ${!focado ? 'hidden-animation' : ''} ${error ? 'error' : ''}`}>
           {lettersClicked.join('')}
         </strong>
