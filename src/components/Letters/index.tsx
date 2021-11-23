b625f1d5a7f9f83ea837213bc8447fefea2ec641
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-11-23 08:38:53 -0300

    focus done

--- src/components/Letters/index.tsx
+++ src/components/Letters/index.tsx
@@ -5,17 +5,23 @@ import './styles.scss';
 const Letters: React.FC = () => {
   const [focado, setFocado] = useState(true)
   const [lettersClicked, setLettersClicked] = useState<string[]>([]);
+  const [error, setError] = useState(false);
   const letterInput = document.querySelector('.letter-input') as HTMLInputElement
   const textContent = document.querySelector('.letters-box')?.textContent
 
   const letterClick = (e) => {
-    console.log(e.charCode, 'asdasd');
+    console.log(e.charCode, 'tecla');
     const letterClicked = String.fromCharCode(e.charCode)
+
     if (textContent?.split('')[lettersClicked.length] === letterClicked) {
       const lettersClickedUpdate = lettersClicked
       lettersClickedUpdate.push(letterClicked)
       setLettersClicked([...lettersClickedUpdate])
+      error && setError(false)
+      return;
     }
+
+    setError(true)
   }
 
   const focusOnInput = () => {
@@ -36,12 +42,11 @@ const Letters: React.FC = () => {
 
   return (
     <div className='letters-container'>
-      <input onKeyPress={letterClick} onBlur={loseFocusInput} onFocus={focusOnInput} autoFocus className='letter-input'/>
       <p className='letters letters-box' onClick={() => letterInput?.focus()}>
+        <input onKeyPress={letterClick} readOnly onBlur={loseFocusInput} onFocus={focusOnInput} autoFocus className='letter-input'/>
         Aqui vai a palavra para testarmos nossa velocidade de digitação. Basta ir copiando esse texto. Aqui vai a palavra para testarmos nossa velocidade de digitação. Basta ir copiando esse texto. AAAAAAAAAAAAAAAAA dsfdsf sdfsdfsf dsf sdfs fdsf  sad asdasdasdasdas asd as as s s f l
-        <strong className={`letters letters-typed ${focado && 'hidden-animation'}`}>
-          {/* {lettersClicked.map(letter => (letter))} */}
-          {/* Aqui vai a palavra para */}
+        <strong className={`letters letters-typed ${!focado ? 'hidden-animation' : ''} ${error ? 'error' : ''}`}>
+          {lettersClicked.join('')}
         </strong>
       </p>
     </div>
